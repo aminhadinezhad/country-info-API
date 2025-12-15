f23d8e3ee81bcd35984f3d12a374c48e7bb642f9
Amin <mohammadaminhadinezhad@gmail.com>
2025-12-15 14:35:10 +0330

    develop project features (neighbour countries)

--- script.js
+++ script.js
@@ -4,7 +4,60 @@ const formBtn = document.querySelector('.form-btn');
 const countriesContainer = document.querySelector('.countries-container');
 const countries = [];
 
-const request = new XMLHttpRequest();
+const renderCountry = function (data, className) {
+  const html = ` <article class="country ${className}">
+                <img class="country__img" src="${data.flags.png}" />
+                <div class="country__data">
+                  <h3 class="country__name">نام کشور: ${
+                    Object.values(data.name)[0]
+                  }</h3>
+                  <h4 class="country__region">قاره: ${data.region}</h4>
+                  <p class="country__row">جمعیت: ${
+                    data.population
+                  }<span>👫</span></p>
+                  <p class="country__row">زبان رسمی: ${Object.values(
+                    data.languages
+                  )}<span>🗣️</span></p>
+                  <p class="country__row">واحد پول: ${
+                    Object.values(data.currencies)[0].name
+                  }<span>💰</span></p>
+                </div>
+              </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://restcountries.com/v3.1/name/${country}?fullText=true`
+  );
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    renderCountry(data);
+
+    const createNewReq = function (code) {
+      const request = new XMLHttpRequest();
+      request.open('GET', `https://restcountries.com/v3.1/alpha/${code}`);
+      request.send();
+      request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText);
+        renderCountry(data, 'neighbour');
+      });
+    };
+
+    const codes = data.borders;
+    codes.forEach(code => {
+      createNewReq(code);
+    });
+  });
+};
+
+request = new XMLHttpRequest();
 request.open('GET', 'https://restcountries.com/v3.1/all?fields=name');
 request.send();
 request.addEventListener('load', function () {
@@ -22,39 +75,8 @@ request.addEventListener('load', function () {
       alert('کشوری با این نام یافت نشد!');
     }
     if (countries.includes(country.toLowerCase())) {
-      request.open(
-        'GET',
-        `https://restcountries.com/v3.1/name/${country}?fullText=true`
-      );
-      request.send();
-
-      request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
-        const html = ` <article class="country">
-              <img class="country__img" src="${data.flags.png}" />
-              <div class="country__data">
-                <h3 class="country__name">نام کشور: ${
-                  Object.values(data.name)[0]
-                }</h3>
-                <h4 class="country__region">قاره: ${data.region}</h4>
-                <p class="country__row">جمعیت: ${
-                  data.population
-                }<span>👫</span></p>
-                <p class="country__row">زبان رسمی: ${Object.values(
-                  data.languages
-                )}<span>🗣️</span></p>
-                <p class="country__row">واحد پول: ${
-                  Object.values(data.currencies)[0].name
-                }<span>💰</span></p>
-              </div>
-            </article>`;
-
-        countriesContainer.insertAdjacentHTML('afterbegin', html);
-
-        formContainer.style.display = 'none';
-        countriesContainer.style.opacity = 1;
-      });
+      formContainer.style.display = 'none';
+      getCountryAndNeighbour(country);
     }
   });
 });
